a3098e105c522e029090efe4fef81bb0f338ffab
neb <nebbbnn@gmail.com>
2025-07-01 13:59:40 -0400

    palette base

--- app/src/main/java/com/example/group_project/Palette.kt
+++ app/src/main/java/com/example/group_project/Palette.kt
@@ -1,4 +1,11 @@
 package com.example.group_project
 
 class Palette {
+    private lateinit var palette : Array<Couleur>
+    private lateinit var paletteName : String
+
+    constructor(palette : Array<Couleur>, paletteName : String) {
+        this.palette = palette
+        this.paletteName = paletteName
+    }
 }
--- app/src/main/java/com/example/group_project/PaletteUtils.kt
+++ app/src/main/java/com/example/group_project/PaletteUtils.kt
@@ -1,4 +1,32 @@
 package com.example.group_project
 
 class PaletteUtils {
+    private var S_DELTA = 50
+    private var H_DELTA = 30
+    private var V_DELTA = 25
+
+
+    // generate one color palette from an array of colors
+    fun generatePalette(colors : Array<Couleur>) : Palette {
+        var generatedPalette = Palette(colors, "")
+
+        return generatedPalette
+    }
+
+    // generate multiple color palette from an array of colors
+    fun generatePalette(colors : Array<Couleur>, palettesToGenerate : Int) : Array<Palette> {
+        var generatedPalettes : Array<Palette> = Array<Palette>(6, { i -> generatePalette(colors) })
+
+        return generatedPalettes
+    }
+
+    // take palette string and convert it to a Array<Couleur>
+    // ex: “(255,0,9),black,green” --> [ Couleur(red), Couleur(black), Couleur(green), null, null, null ]
+
+    fun paletteStringToCouleurArray(paletteString : String) : Array<Couleur> {
+        var couleurArray = Array<Couleur>(6, { i -> Couleur() })
+
+        return couleurArray
+    }
+
 }
